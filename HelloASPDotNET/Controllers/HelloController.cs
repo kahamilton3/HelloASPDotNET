b621862de7a1e64336650c9cdd3640123ebe8654
Kirby <kafracas@gmail.com>
2020-11-09 15:45:02 -0600

    completed chapter 13 reading practice

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            ViewBag.person = name;
+            return View();
+        }
+    }
+}
