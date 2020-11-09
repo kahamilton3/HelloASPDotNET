13f5af9446a3ffa712374b0ad41d755f79357c1d
Kirby <kafracas@gmail.com>
2020-11-09 13:52:53 -0600

    Add project files.

---
+++ HelloASPDotNET/Controllers/HelloController1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            string html = "<h1>Hello World!</h1>";
+            return Content(html, "text/html");
+        }
+    }
+}
